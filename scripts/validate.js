8e6a8573e09e8247c54d5bf33ccaa694c5ec4105
Yulia Markowa <markowayulia@yandex.ru>
2021-02-17 17:47:04 +0300

    aded classes

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,7 +4,7 @@ enableValidation({
     submitButtonSelector: ".popup__submit",
     inactiveButtonClass: "popup__submit_disabled",
     inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible",
+    errorClass: "popup__input-error_visible",
   });
   
   const showInputError = (formElement, inputElement, errorMessage) => {
