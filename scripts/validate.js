161acc3c3a0e3790a26f359bc0e3911ed6108bad
Yulia Markowa <markowayulia@yandex.ru>
2021-02-19 15:16:10 +0300

    first review

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,42 +1,21 @@
 const showInputError = (
-  formElement,
-  inputElement,
-  inputErrorClass,
-  errorClass,
-  errorMessage
-) => {
+  formElement, inputElement, inputErrorClass, errorClass, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(inputErrorClass);
   errorElement.classList.add(errorClass);
   errorElement.textContent = errorMessage;
 };
 
-const hideInputError = (
-  formElement,
-  inputElement,
-  inputErrorClass,
-  errorClass
-) => {
+const hideInputError = (formElement, inputElement, inputErrorClass, errorClass) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove(inputErrorClass);
   errorElement.classList.remove(errorClass);
   errorElement.textContent = "";
 };
 
-const checkInputValidity = (
-  formElement,
-  inputElement,
-  inputErrorClass,
-  errorClass
-) => {
+const checkInputValidity = (formElement, inputElement, inputErrorClass, errorClass) => {
   if (!inputElement.validity.valid) {
-    showInputError(
-      formElement,
-      inputElement,
-      inputErrorClass,
-      errorClass,
-      inputElement.validationMessage
-    );
+    showInputError(formElement, inputElement, inputErrorClass, errorClass, inputElement.validationMessage);
   } else {
     hideInputError(formElement, inputElement, inputErrorClass, errorClass);
   }
@@ -58,16 +37,7 @@ const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
   }
 };
 
-const setEventListeners = (
-  formElement,
-  {
-    inputSelector,
-    submitButtonSelector,
-    inactiveButtonClass,
-    inputErrorClass,
-    errorClass,
-  }
-) => {
+const setEventListeners = (formElement, {inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass}) => {
   const inputList = Array.from(formElement.querySelectorAll(inputSelector));
   const buttonElement = formElement.querySelector(submitButtonSelector);
 
@@ -75,12 +45,7 @@ const setEventListeners = (
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
-      checkInputValidity(
-        formElement,
-        inputElement,
-        inputErrorClass,
-        errorClass
-      );
+      checkInputValidity(formElement, inputElement, inputErrorClass, errorClass);
 
       toggleButtonState(inputList, buttonElement, inactiveButtonClass);
     });
