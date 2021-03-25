c04d388ac00c3688f7a076208a3c5680329f5150
Yulia Markowa <markowayulia@yandex.ru>
2021-03-25 14:54:23 +0300

    without submits

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,16 +1,18 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-    constructor(popupSelector) {
+  constructor(popupSelector) {
     super(popupSelector);
-    this._popupPhotoImage = document.querySelector(".popup__image");
-    this._popupPhotoFigcaption = document.querySelector(".popup__figcaption");
-    }
+    this._popupPhotoImage = this._popupElement.querySelector(".popup__image");
+    this._popupPhotoFigcaption = this._popupElement.querySelector(
+      ".popup__figcaption"
+    );
+  }
 
-    open(link, figcaption) {
-        this._popupPhotoImage.src = link;
-        this._popupPhotoFigcaption.textContent = figcaption.textContent;
-        this._popupPhotoImage.alt = `Изображение места: ${figcaption.textContent}`;
-        super.open();
-    }
+  open(link, figcaption) {
+    this._popupPhotoImage.src = link;
+    this._popupPhotoFigcaption.textContent = figcaption.textContent;
+    this._popupPhotoImage.alt = `Изображение места: ${figcaption.textContent}`;
+    super.open();
+  }
 }
