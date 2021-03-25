945d091c5937c4c558cf86cfe4151f78186f369e
Yulia Markowa <markowayulia@yandex.ru>
2021-03-25 07:42:55 +0300

    add indexjs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,24 +33,22 @@ export const initialCards = [
     errorClass: "popup__input-error_active",
   };
 
-
-export const cardsContainer = ".cards";  
+export const cardsContainer = document.querySelector(".cards");  
 export const formElementEdit = document.querySelector(".popup__form-edit");
 export const formElementNewCard = document.querySelector(".popup__form-add");
 
 export const editProfileButton = document.querySelector(".profile__edit-button");
 export const addButton = document.querySelector(".profile__add-button");
 
-export const nameInput = document.querySelector(".popup__input[name=name]");
-export const aboutInput = document.querySelector(".popup__input[name=about]");
+export const profileName = document.querySelector(".profile__name");
+export const profileCaption = document.querySelector(".profile__caption");
 
+export const popupProfile = document.querySelector(".popup_type_profile");
+export const popupNewCard = document.querySelector(".popup_type_new-card");
+export const popupPhoto = document.querySelector(".popup_type_image");
+//export const nameInput = document.querySelector(".popup__input[name=name]");
+//export const aboutInput = document.querySelector(".popup__input[name=about]");
 
-//export const popupProfile = document.querySelector(".popup_type_profile");
-//export const popupNewCard = document.querySelector(".popup_type_new-card");
-//export const popupPhoto = document.querySelector(".popup_type_image");
 //export const overlays = document.querySelectorAll(".popup");
- 
 //export const caption = document.querySelector(".popup__input[name=caption]");
 //export const link = document.querySelector(".popup__input[name=link]");
-//export const profileName = document.querySelector(".profile__name");
-//export const profileCaption = document.querySelector(".profile__caption");
